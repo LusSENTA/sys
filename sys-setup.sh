6650868857a7657db38bdd3dd0785d570566e1b0
LusSenta <84939012+LusSenta@users.noreply.github.com>
2021-05-28 11:04:39 -0700

    Added sys-setup.sh version 3

--- sys-setup.sh
+++ sys-setup.sh
@@ -1,6 +1,6 @@
+cd ~
 sudo apt update
 sudo apt upgrade
-sudo apt update
 sudo apt install krita gimp vlc tor audacity qbittorrent git timeshift kdenlive ffmpeg youtube-dl -y
 mkdir software
 mkdir archive
