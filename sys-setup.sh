82d7599895f95db85aade81b4441b1020b863c87
LusSenta <84939012+LusSenta@users.noreply.github.com>
2021-05-31 13:42:41 -0700

    Added sys-setup.sh version 4

--- sys-setup.sh
+++ sys-setup.sh
@@ -4,3 +4,8 @@ sudo apt upgrade
 sudo apt install krita gimp vlc tor audacity qbittorrent git timeshift kdenlive ffmpeg youtube-dl -y
 mkdir software
 mkdir archive
+mkdir files
+cd software
+git clone https://github.com/githacktools/TorghostNG
+cd TorghostNG
+sudo python3 install.py
